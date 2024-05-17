752586325c2e445bec8923796bc74e16307b3bbc
unknown <smija@windfallproductions.org>
2024-05-17 13:36:36 +0530

    initial commit - card one and two design

--- src/components/Cards/CardTwo.js
+++ src/components/Cards/CardTwo.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import './CardTwo.scss'
 
 const CardTwo = () => {
   return (
-    <div>CardTwo</div>
+    <div class="cardTwo">
+      <img src="https://picsum.photos/200/300" />
+      <div class="container-card2">
+        <h5>7 mins read</h5>
+        <h3>
+          Eucharistic
+          miracles and scientific facts.
+        </h3>
+      </div>
+    </div>
   )
 }
 
